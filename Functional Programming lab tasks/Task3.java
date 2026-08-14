eca12806ab760badc9d985fb833b284dddbe00ce
vtu30063 <vtu30063@veltech.edu.in>
2026-08-14 12:25:04 +0530

    Update Task3.java

--- Functional Programming lab tasks/Task3.java
+++ Functional Programming lab tasks/Task3.java
@@ -1,31 +1,35 @@
+
 import java.util.*;
 
-public class Task3{
+public class Task4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
-        int[] nums = new int[n];
+        int current = sc.nextInt();
+        int max = current;
 
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
+        for (int i = 1; i < n; i++) {
+            int value = sc.nextInt();
+
+            current = Math.max(value, current + value);
+            max = Math.max(max, current);
         }
 
-        int[] result = Arrays.stream(nums).collect(
-            () -> new int[]{0, Integer.MIN_VALUE},
-            (a, x) -> {
-                a[0] = Math.max(x, a[0] + x);
-                a[1] = Math.max(a[1], a[0]);
-            },
-            (a, b) -> {
-                a[0] = Math.max(a[0], b[0]);
-                a[1] = Math.max(a[1], b[1]);
-            }
-        );
-
-        System.out.println(result[1]);
+        System.out.println(max);
 
         sc.close();
     }
 }
+   //input:8
+   
+   -2
+   -3
+   4
+   -1
+   -2
+   1
+   5
+   -3
+//output: 7
