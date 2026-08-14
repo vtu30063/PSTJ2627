6f4f60b8b1c8c5b8c02ec1363660b9402259a67e
vtu30063 <vtu30063@veltech.edu.in>
2026-08-14 12:09:31 +0530

    Refactor Task2.java to Main.java with improved logic

--- Functional Programming lab tasks/Task2.java
+++ Functional Programming lab tasks/Task2.java
@@ -1,39 +1,40 @@
 import java.util.*;
 import java.util.stream.*;
 
-public class Task2 {
+public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        List<String> data = new ArrayList<>();
+
+        List<String> sensors = new ArrayList<>();
+        List<Double> temps = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            data.add(sc.next() + " " + sc.nextDouble());
+            sensors.add(sc.next());
+            temps.add(sc.nextDouble());
         }
 
-        data.stream()
-            .map(s -> s.split(" "))
-            .filter(a -> Double.parseDouble(a[1]) > 50)
-            .collect(Collectors.groupingBy(
-                a -> a[0],
-                Collectors.averagingDouble(
-                    a -> Double.parseDouble(a[1])
-                )
-            ))
-            .entrySet()
-            .stream()
-            .sorted(
-                Map.Entry.<String, Double>
-                comparingByValue()
-                .reversed()
-            )
-            .forEach(
-                e -> System.out.println(
-                    e.getKey() + " " + e.getValue()
-                )
-            );
+        Map<String, Double> result = IntStream.range(0, n)
+                .filter(i -> temps.get(i) > 50)
+                .mapToObj(i -> new AbstractMap.SimpleEntry<>(
+                        sensors.get(i), temps.get(i)))
+                .collect(Collectors.groupingBy(
+                        Map.Entry::getKey,
+                        Collectors.averagingDouble(Map.Entry::getValue)
+                ));
+
+        result.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Double>comparingByValue().reversed())
+                .forEach(e -> System.out.println(e.getKey() + " " + e.getValue()));
 
         sc.close();
     }
 }
+//output:
+60
+80
+70
+90
